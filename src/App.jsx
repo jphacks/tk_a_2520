febbfea9a197a8c5b2fa7d8125856313cd99d7e1
tapen25 <ayukky6245@gmail.com>
2025-10-11 17:37:56 +0900

    投稿フォームのコンポーネントを追加

--- src/App.jsx
+++ src/App.jsx
@@ -14,14 +14,14 @@ function App() {
       {/* ページ移動用のテストナビゲーション */}
       <nav style={{ padding: '10px', backgroundColor: '#eee', marginBottom: '20px' }}>
         <strong>ページ移動テスト:</strong>
-        <Link to="/" style={{ marginLeft: '15px' }}>投稿フォーム</Link>
+        <Link to="/post" style={{ marginLeft: '15px' }}>投稿フォーム</Link>
         <Link to="/post/1" style={{ marginLeft: '15px' }}>投稿詳細</Link>
         <Link to="/map" style={{ marginLeft: '15px' }}>情報マップ</Link>
       </nav>
 
       {/* URLに応じて表示するコンポーネントを切り替える設定 */}
       <Routes>
-        <Route path="/" element={<PostForm />} />
+        <Route path="/post" element={<PostForm />} />
         <Route path="/post/1" element={<PostDetail />} />
         <Route path="/map" element={<InfoMap />} />
       </Routes>
