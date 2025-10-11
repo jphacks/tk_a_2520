67893154a412c6e28f2466359587da8a953f9231
tapen25 <ayukky6245@gmail.com>
2025-10-11 15:35:00 +0900

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// ↓ここにリポジトリ名を指定
 export default defineConfig({
   plugins: [react()],
+  base: '/tk_a_2520/', 
 })
