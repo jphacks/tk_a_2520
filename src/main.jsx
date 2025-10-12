20f401798eb0efe4869c8a8da04211032077c68e
tapen25 <ayukky6245@gmail.com>
2025-10-12 11:44:23 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { LoadScript } from '@react-google-maps/api'; // LoadScriptをインポート
-import WebApp from './WebApp';
-import './WebApp.css';
+import App from './App';
+//import './WebApp.css';
 
 // アプリケーション全体で使用するライブラリを定義（必要に応じて変更）
 const libraries = ["maps", "places"];
@@ -19,7 +19,7 @@ root.render(
       libraries={libraries}
     >
       <BrowserRouter>
-        <WebApp />
+        <App />
       </BrowserRouter>
     </LoadScript>
   </React.StrictMode>
